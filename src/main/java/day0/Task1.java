c183ed997fc9c947feeb656c303575f5a51af427
Liudmyla Yusupova <lydmilakruch@gmail.com>
2021-02-21 20:39:18 +0100

    Task1 small fix

--- src/main/java/day0/Task1.java
+++ src/main/java/day0/Task1.java
@@ -2,7 +2,7 @@ package day0;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello word");
+        System.out.println("Hello world");
         
     }
 }
