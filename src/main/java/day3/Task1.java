7f344e4dfdf035c0e1d591f3c12bb835b62b6487
Liudmyla Yusupova <lydmilakruch@gmail.com>
2021-02-25 09:41:35 +0100

    Fix for Task1 from Day3

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -6,25 +6,28 @@ public class Task1 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String city = scanner.nextLine();
 
-        switch (city) {
-            case "Москва", "Владивосток", "Ростов":
-                System.out.println("Россия");
+        for (; ; ) {
+            String city = scanner.nextLine();
+            if (city.equals("Stop")){
                 break;
-            case "Рим", "Милан", "Турин":
-                System.out.println("Италия");
-                break;
-            case "Ливерпуль", "Манчестер", "Лондон":
-                System.out.println("Англия");
-                break;
-            case "Берлин", "Мюнхен", "Кёльн":
-                System.out.println("Германия");
-                break;
-            case "Stop":
-                break;
-            default:
-                System.out.println("Неизвестная страна");
+            }
+            switch (city) {
+                case "Москва", "Владивосток", "Ростов":
+                    System.out.println("Россия");
+                    break;
+                case "Рим", "Милан", "Турин":
+                    System.out.println("Италия");
+                    break;
+                case "Ливерпуль", "Манчестер", "Лондон":
+                    System.out.println("Англия");
+                    break;
+                case "Берлин", "Мюнхен", "Кёльн":
+                    System.out.println("Германия");
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
+            }
         }
     }
 }
