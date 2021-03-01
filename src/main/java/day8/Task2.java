877ba76189faa39c267d9d537178dc6cdd7c4e89
Liudmyla Yusupova <lydmilakruch@gmail.com>
2021-03-01 09:46:06 +0100

    Solution for Tasks of day8

--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -3,5 +3,29 @@ package day8;
 public class Task2 {
     public static void main(String[] args) {
 
+        long start = System.currentTimeMillis();
+        StringBuilder sbNumbers = new StringBuilder();
+        for (int i = 0; i <= 20; i++) {
+            sbNumbers.append(i).append(" ");
+
+        }
+        System.out.println(sbNumbers);
+        long finish = System.currentTimeMillis();
+        long timeConsumedMillis = finish - start;
+        System.out.println(timeConsumedMillis);
+
+        long start2 = System.currentTimeMillis();
+        String stringNumbers;
+        for (int i = 0; i <= 20; i++) {
+            stringNumbers = i + " ";
+            System.out.print(stringNumbers);
+        }
+        long finish2 = System.currentTimeMillis();
+        long timeConsumedMillis2 = finish2 - start2;
+        System.out.println(timeConsumedMillis2);
+
     }
+
 }
+
+
